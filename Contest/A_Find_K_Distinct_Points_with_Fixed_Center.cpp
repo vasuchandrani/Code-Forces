4227fb98cf1987d5767aa8b567b435ce198503b0
vasuchandrani <vasuchandrani1118@gmail.com>
2024-08-20 23:03:05 +0530

    codes

---
+++ Contest/A_Find_K_Distinct_Points_with_Fixed_Center.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int Xc, Yc, k;
+        cin>>Xc>>Yc>>k;
+
+        for(int i=0;i<k;i++){
+            
+        }
+    }
+}
